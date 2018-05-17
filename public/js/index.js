9094675b4de69bf5889fea445be844a64d40726e
Semen Vodyannikov <Semen_Vodyannikov@epam.com>
2018-05-17 11:18:33 -0400

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,28 @@ jQuery('#message-form').on('submit', function (e) {
 
     });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Вычисление по IP не поддерживается Вашим браузером.');
+    }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Не удаётся вычислить по IP.');
+    });
+});
+
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">Я нахожусь здесь</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
